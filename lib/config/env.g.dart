c8d8b4107eadff83de507763f80e6a4f1c73084d
Felipe Ferreira <feferreira.sf@gmail.com>
2025-08-14 19:48:58 -0300

    chore: add environment configuration with API keys and backend URL

---
+++ lib/config/env.g.dart
@@ -0,0 +1,120 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// generated_from: .env
+final class _Env {
+  static const List<int> _enviedkeygoogleApiKey = <int>[
+    620193977,
+    2739133712,
+    276751058,
+    4200314022,
+    686851993,
+    26646181,
+    2779008861,
+    2962222566,
+    2489769891,
+    2138740973,
+    1719194728,
+    283960362,
+    2725445382,
+    3062676019,
+    246841839,
+    2278032950,
+    945018466,
+  ];
+
+  static const List<int> _envieddatagoogleApiKey = <int>[
+    620193984,
+    2739133823,
+    276751015,
+    4200314068,
+    686852038,
+    26646212,
+    2779008813,
+    2962222479,
+    2489769980,
+    2138740870,
+    1719194637,
+    283960403,
+    2725445465,
+    3062676059,
+    246841738,
+    2278032964,
+    945018375,
+  ];
+
+  static final String googleApiKey = String.fromCharCodes(
+    List<int>.generate(
+      _envieddatagoogleApiKey.length,
+      (int i) => i,
+      growable: false,
+    ).map((int i) => _envieddatagoogleApiKey[i] ^ _enviedkeygoogleApiKey[i]),
+  );
+
+  static const String backendBaseUrl = 'your_backend_base_url_here';
+
+  static const List<int> _enviedkeytmdbApiKey = <int>[
+    423855766,
+    3553504195,
+    1198184056,
+    3320731183,
+    3932703515,
+    2833416922,
+    3878759590,
+    2166847077,
+    3086782338,
+    3399751276,
+    3968276256,
+    364779162,
+    2817280269,
+    510561520,
+    2239559417,
+    1459028528,
+    1727669033,
+    228588526,
+    169706454,
+    2541311697,
+    3992564785,
+    1416835817,
+  ];
+
+  static const List<int> _envieddatatmdbApiKey = <int>[
+    423855855,
+    3553504172,
+    1198183949,
+    3320731229,
+    3932703556,
+    2833416878,
+    3878759627,
+    2166846977,
+    3086782432,
+    3399751219,
+    3968276289,
+    364779242,
+    2817280356,
+    510561455,
+    2239559314,
+    1459028565,
+    1727669072,
+    228588465,
+    169706430,
+    2541311668,
+    3992564803,
+    1416835724,
+  ];
+
+  static final String tmdbApiKey = String.fromCharCodes(
+    List<int>.generate(
+      _envieddatatmdbApiKey.length,
+      (int i) => i,
+      growable: false,
+    ).map((int i) => _envieddatatmdbApiKey[i] ^ _enviedkeytmdbApiKey[i]),
+  );
+}
